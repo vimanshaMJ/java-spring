ee84c0bedc3ea44089e5b1c6c1186ceaecd7e538
vimansha <vimanshamadushani88@gmail.com>
2024-09-28 22:36:23 +0530

    feat: array II

--- Demo.java
+++ Demo.java
@@ -1,14 +1,26 @@
 
 public class Demo {
     public static void main(String a[]) {
-        int nums[] = {3,7,5,9};
-        System.out.println(nums[1]);
+        int nums[][] = new int[3][4]; // multidimensional array
 
-        nums[1] = 8;
-        System.out.println(nums[1]);
+        int nums1[][] = new int[3][]; // jagged array, individually specified inner array sizes
+        nums1[0] = new int[3];
+        nums1[1] = new int[4];
+        nums1[2] = new int[2];
 
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<4; j++) {
+                nums[i][j] = (int)(Math.random()*10);
+            }
+            
+        }
+        
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<4; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
