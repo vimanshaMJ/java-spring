a84cdba51dc480ee41aaf86d19b9cc3c3a15f78f
vimansha <vimanshamadushani88@gmail.com>
2024-09-28 21:04:34 +0530

    feat: array

--- Demo.java
+++ Demo.java
@@ -1,20 +1,14 @@
-class Computer {
-    public void playMusic() {
-        System.out.println("Music playing...");
-    }
-
-    public String getPen(int cost) {
-        if (cost >= 10)
-            return "Pen";
-        return "Nothing";
-    }
-}
 
 public class Demo {
     public static void main(String a[]) {
-        Computer obj = new Computer();
-        obj.playMusic();
-        String str = obj.getPen(8);
-        System.out.println(str);
+        int nums[] = {3,7,5,9};
+        System.out.println(nums[1]);
+
+        nums[1] = 8;
+        System.out.println(nums[1]);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
     }
 }
